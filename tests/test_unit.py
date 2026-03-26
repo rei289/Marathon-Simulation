4268386c3b438676eb04ee498fde20d4dcae7734
rei289 <rei.katsuta@gmail.com>
2026-03-26 01:06:31 +0100

    added more unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -67,16 +67,36 @@ def sim(sim_cfg: SimConfig, params: Params) -> MonteCarloSimulation:
     return MonteCarloSimulation(sim_cfg, strat, df_input=df_input, csv_data=None, json_data=None)
 
 # overall simulation tests
-def test_simulation_runs(sim: MonteCarloSimulation) -> None:
+def test_finish_time(sim: MonteCarloSimulation) -> None:
     """Test that the simulation runs without errors and produces a finish time."""
     sim.run()
     assert len(sim.finish_time) == 1
-    assert sim.finish_time[0] > 0
+    assert np.all(sim.finish_time > 0.0 or np.isnan(sim.finish_time))
 
 def test_time_positive(sim: MonteCarloSimulation) -> None:
     """Test that the finish time is positive."""
     sim.run()
     assert np.all(sim.time_elapsed >= 0.0)
+
+def test_distance_positive(sim: MonteCarloSimulation) -> None:
+    """Test that the distance covered is positive."""
+    sim.run()
+    assert np.all(sim.distance_covered >= 0.0)
+
+def test_energy_non_negative(sim: MonteCarloSimulation) -> None:
+    """Test that the energy is non-negative."""
+    sim.run()
+    assert np.all(sim.energy >= 0.0)
+
+def test_velocity_non_negative(sim: MonteCarloSimulation) -> None:
+    """Test that the velocity is non-negative."""
+    sim.run()
+    assert np.all(sim.velocity >= 0.0)
+
+def test_energy_lower_than_initial(sim: MonteCarloSimulation) -> None:
+    """Test that the energy is always lower than or equal to the initial energy."""
+    sim.run()
+    assert np.all(sim.energy <= sim.E0_values)
 
 
 # pacing strategy tests
